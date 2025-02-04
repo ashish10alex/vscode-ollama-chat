05b5dda49a63035bc273e08e1dbe6563df35bd08
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2025-02-04 13:27:08 +0000

    chore: modify system prompt

--- src/extension.ts
+++ src/extension.ts
@@ -52,7 +52,7 @@ export function activate(context: vscode.ExtensionContext) {
 		panel.webview.onDidReceiveMessage(async(message: any) => {
 			let responseText = "";
 			if(message.command === 'chat'){
-				const systemPromt = { role: 'system', content: 'give me links in markdown format [text_of_link](address_to_resource) for any resource that you wnat to point me to from where you inferred the answer'};
+				const systemPromt = { role: 'system', content: 'give me links in markdown format [text_of_link](address_to_resource) for any resource that you wnat to point me to from where you inferred the answer add a header resources in markdown format above the links'};
 				const userPromt = { role: 'user', content: message.question};
 				const response = await ollama.chat(
 					{
