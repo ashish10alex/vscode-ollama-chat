7a54a34415579265b3e68fab01ec165687ca57b9
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2025-02-06 00:36:10 +0000

    feat: improve new chat button ui

--- src/chat.ts
+++ src/chat.ts
@@ -116,11 +116,13 @@ return /*html*/ `
 
     <button 
         id="refreshBtn" 
-        class="absolute top-4 right-4 p-2 bg-[#0066AD] text-white rounded-full hover:bg-[#0077CC] transition-colors disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-[#0066AD]"
+        class="absolute top-4 right-4 flex items-center gap-2 px-3 py-2 bg-[#0066AD] text-white rounded-lg hover:bg-[#0077CC] transition-colors disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-[#0066AD]"
+        title="New Chat"
     >
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
+            <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
         </svg>
+        <span class="text-sm font-medium">New Chat</span>
     </button>
 
       <!-- Error message for Ollama CLI not installed -->
